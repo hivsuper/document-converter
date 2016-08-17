c37a50e0f8c7ad408c6d1592ee5fa4d4f6b67e96
Super Li <xupenglee@gmail.com>
2016-08-17 11:22:58 +0800

    modify scope

--- src/main/java/org/lxp/converter/impl/JodPDFconverter.java
+++ src/main/java/org/lxp/converter/impl/JodPDFconverter.java
@@ -84,7 +84,7 @@ public class JodPDFconverter extends PDFConverter {
     return returnVo;
   }
 
-  public static void startService() {
+  private void startService() {
     DefaultOfficeManagerConfiguration configuration = new DefaultOfficeManagerConfiguration();
     try {
       LOG.info("start service....");
@@ -101,7 +101,7 @@ public class JodPDFconverter extends PDFConverter {
     }
   }
 
-  public static void stopService() {
+  private void stopService() {
     LOG.info("close office converting service....");
     if (officeManager != null) {
       officeManager.stop();
